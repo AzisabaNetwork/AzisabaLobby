6373d982d3d2214bee3c806dbd963346d4a6313b
acrylic-style <me@acrylicstyle.xyz>
2021-10-24 23:05:35 +0900

    Add allow-flight-groups

--- src/main/java/net/azisaba/lobby/AzisabaLobby.java
+++ src/main/java/net/azisaba/lobby/AzisabaLobby.java
@@ -1,16 +1,51 @@
 package net.azisaba.lobby;
 
+import net.azisaba.lobby.listeners.CheckJoinListener;
 import net.azisaba.lobby.listeners.VoidToSpawnListener;
+import net.luckperms.api.LuckPerms;
+import net.luckperms.api.LuckPermsProvider;
+import net.luckperms.api.context.ImmutableContextSet;
+import net.luckperms.api.model.user.User;
+import net.luckperms.api.query.QueryOptions;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class AzisabaLobby extends JavaPlugin {
+  private LuckPerms luckPerms;
 
   @Override
   public void onEnable() {
+    getConfig().options().copyDefaults();
     Bukkit.getPluginManager().registerEvents(new VoidToSpawnListener(this), this);
+    Bukkit.getPluginManager().registerEvents(new CheckJoinListener(this), this);
+    Bukkit.getPluginCommand("azisabalobby").setExecutor(new AzisabaLobbyCommand(this));
 
+    // late init task
+    Bukkit.getScheduler().runTaskLater(this, () -> {
+      // Get LuckPerms API
+      luckPerms = LuckPermsProvider.get();
+    }, 1);
     Bukkit.getLogger().info(getName() + " enabled.");
+  }
+
+  public LuckPerms getLuckPerms() {
+    return luckPerms;
+  }
+
+  public boolean isAllowedToFly(Player player) {
+    return getConfig()
+            .getStringList("allow-flight-groups")
+            .stream()
+            .anyMatch(name -> hasGroup(player, name));
+  }
+
+  public boolean hasGroup(Player player, String groupName) {
+    User user = getLuckPerms().getPlayerAdapter(Player.class).getUser(player);
+    String server = getLuckPerms().getServerName();
+    return user.getInheritedGroups(QueryOptions.contextual(ImmutableContextSet.of("server", server)))
+            .stream()
+            .anyMatch(group -> group.getName().equals(groupName));
   }
 
   @Override
