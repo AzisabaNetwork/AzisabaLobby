954fde77db922b14ad20550340af1f4d2b9ff7bf
acrylic-style <me@acrylicstyle.xyz>
2021-10-24 23:18:01 +0900

    ✨ add azisabalobby.allow-<creative|survival|spectator>-mode permission node

--- src/main/java/net/azisaba/lobby/listeners/CheckJoinListener.java
+++ src/main/java/net/azisaba/lobby/listeners/CheckJoinListener.java
@@ -1,5 +1,7 @@
 package net.azisaba.lobby.listeners;
 
+import org.bukkit.GameMode;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -7,6 +9,19 @@ import org.bukkit.event.player.PlayerJoinEvent;
 public class CheckJoinListener implements Listener {
     @EventHandler
     public void checkOnJoin(PlayerJoinEvent e) {
+        checkGameMode(e.getPlayer());
         e.getPlayer().setAllowFlight(e.getPlayer().hasPermission("azisabalobby.allow-flight"));
+    }
+
+    private static void checkGameMode(Player player) {
+        if (player.getGameMode() == GameMode.CREATIVE && !player.hasPermission("azisabalobby.allow-creative-mode")) {
+            player.setGameMode(GameMode.ADVENTURE);
+        }
+        if (player.getGameMode() == GameMode.SURVIVAL && !player.hasPermission("azisabalobby.allow-survival-mode")) {
+            player.setGameMode(GameMode.ADVENTURE);
+        }
+        if (player.getGameMode() == GameMode.SPECTATOR && !player.hasPermission("azisabalobby.allow-spectator-mode")) {
+            player.setGameMode(GameMode.ADVENTURE);
+        }
     }
 }
