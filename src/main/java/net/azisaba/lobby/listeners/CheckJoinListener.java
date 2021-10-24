af9a52c8ef806a66ca8c94ada6bb47b71eda0ebb
acrylic-style <me@acrylicstyle.xyz>
2021-10-24 23:13:34 +0900

    reverted almost all changes

--- src/main/java/net/azisaba/lobby/listeners/CheckJoinListener.java
+++ src/main/java/net/azisaba/lobby/listeners/CheckJoinListener.java
@@ -1,18 +1,12 @@
 package net.azisaba.lobby.listeners;
 
-import lombok.RequiredArgsConstructor;
-import net.azisaba.lobby.AzisabaLobby;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
-@RequiredArgsConstructor
 public class CheckJoinListener implements Listener {
-    private final AzisabaLobby plugin;
-
     @EventHandler
     public void checkOnJoin(PlayerJoinEvent e) {
-        boolean canFly = plugin.isAllowedToFly(e.getPlayer());
-        e.getPlayer().setAllowFlight(canFly);
+        e.getPlayer().setAllowFlight(e.getPlayer().hasPermission("azisabalobby.allow-flight"));
     }
 }
