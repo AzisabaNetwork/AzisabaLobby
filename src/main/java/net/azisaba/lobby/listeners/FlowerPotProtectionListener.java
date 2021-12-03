3987ed256079d975657c1c0f90ba80e1c4fe1a53
merunno <merunno92@gmail.com>
2021-12-03 16:26:39 +0900

    [Change] クリックしたアイテムを判定するように変更

--- src/main/java/net/azisaba/lobby/listeners/FlowerPotProtectionListener.java
+++ src/main/java/net/azisaba/lobby/listeners/FlowerPotProtectionListener.java
@@ -1,23 +1,21 @@
 package net.azisaba.lobby.listeners;
 
 import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.inventory.ItemStack;
 
 public class FlowerPotProtectionListener implements Listener {
 
     @EventHandler
     public void onPot(PlayerInteractEvent e) {
         Player player = e.getPlayer();
-        ItemStack item = e.getItem();
+        Block block = e.getClickedBlock();
         if(player.hasPermission("azisabalobby.allow-interact-flower-pot")) return;
-        if(item != null) {
-            if(item.getType() == Material.FLOWER_POT) {
-                e.setCancelled(true);
-            }
+        if(block != null && block.getType() == Material.FLOWER_POT) {
+            e.setCancelled(true);
         }
     }
 }
