55367a514f39a57074d8bbcb1fe07fb5f9923591
merunno <merunno92@gmail.com>
2021-12-03 18:13:54 +0900

    [Remove] 不要なコードの削除

--- src/main/java/net/azisaba/lobby/listeners/HangingProtectionListener.java
+++ src/main/java/net/azisaba/lobby/listeners/HangingProtectionListener.java
@@ -2,6 +2,7 @@ package net.azisaba.lobby.listeners;
 
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Painting;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
@@ -10,8 +11,12 @@ public class HangingProtectionListener implements Listener {
 
     @EventHandler
     public void onHanging(EntityDamageByEntityEvent e) {
-        Entity entity = e.getEntity();
-        if (entity instanceof Painting) e.setCancelled(true);
+        Entity damager = e.getDamager();
+        if (damager instanceof Player) {
+            if (damager.hasPermission("azisabalobby.allow-destroy-painting")) return;
+            Entity entity = e.getEntity();
+            if (entity instanceof Painting) e.setCancelled(true);
+        }
     }
 
 }
