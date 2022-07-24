68d9d75d2f98c07d3dccbfaa4842ee1ce7a3c1a6
m2en <merunno92@gmail.com>
2022-07-24 23:24:46 +0900

    feat: 金リンゴにしてLoreの追加

--- src/main/java/net/azisaba/lobby/util/ParticleMenuItem.java
+++ src/main/java/net/azisaba/lobby/util/ParticleMenuItem.java
@@ -1,5 +1,7 @@
 package net.azisaba.lobby.util;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
@@ -7,10 +9,13 @@ import org.bukkit.inventory.meta.ItemMeta;
 
 public class ParticleMenuItem {
 
-  public static ItemStack phItem = new ItemStack(Material.NETHER_STAR);
+  public static ItemStack phItem = new ItemStack(Material.GOLDEN_APPLE);
   static {
     ItemMeta meta = phItem.getItemMeta();
     meta.setDisplayName(ChatColor.AQUA + "パーティクルメニューを開く");
+    List<String> lores = new ArrayList<>();
+    lores.add(ChatColor.GRAY + "右クリックでパーティクルをつけることができるメニューを開きます!\n");
+    meta.setLore(lores);
     phItem.setItemMeta(meta);
   }
 
