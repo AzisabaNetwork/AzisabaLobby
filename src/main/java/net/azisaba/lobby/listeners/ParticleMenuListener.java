9c946765b4e002e9b096eeba78d87847510bd1e9
m2en <merunno92@gmail.com>
2022-07-25 21:15:05 +0900

    fix: 不要なimport文の削除

--- src/main/java/net/azisaba/lobby/listeners/ParticleMenuListener.java
+++ src/main/java/net/azisaba/lobby/listeners/ParticleMenuListener.java
@@ -1,8 +1,6 @@
 package net.azisaba.lobby.listeners;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import net.azisaba.lobby.AzisabaLobby;
 import org.bukkit.Bukkit;
