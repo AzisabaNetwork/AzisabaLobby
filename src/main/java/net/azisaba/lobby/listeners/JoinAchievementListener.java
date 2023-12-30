395f1a169ca3a496e263d7b597cac6a4c2159eb8
acrylic-style <me@acrylicstyle.xyz>
2023-12-30 18:32:07 +0900

    feat: unlock new year achievement on 1/1

--- src/main/java/net/azisaba/lobby/listeners/JoinAchievementListener.java
+++ src/main/java/net/azisaba/lobby/listeners/JoinAchievementListener.java
@@ -6,6 +6,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
+import java.util.Calendar;
 import java.util.concurrent.TimeUnit;
 
 public class JoinAchievementListener implements Listener {
@@ -20,5 +21,18 @@ public class JoinAchievementListener implements Listener {
                 .sync()
                 .delay(2, TimeUnit.SECONDS)
                 .schedule();
+        Calendar calendar = Calendar.getInstance();
+        boolean isNewYear = calendar.get(Calendar.MONTH) == Calendar.JANUARY && calendar.get(Calendar.DATE) == 1;
+        if (isNewYear) {
+            AzisabaAchievementsProvider.get()
+                    .getScheduler()
+                    .builder(() ->
+                            AzisabaAchievementsProvider.get()
+                                    .getAchievementManager()
+                                    .progressAchievement(e.getPlayer().getUniqueId(), Key.key("azisaba", "general/happy_new_year"), 1))
+                    .sync()
+                    .delay(1, TimeUnit.SECONDS)
+                    .schedule();
+        }
     }
 }
